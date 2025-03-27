98f4353ef76353961ffca48e1c7918ab866f80a5
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2025-03-28 01:06:25 +0530

    Add scraping, parsing, chunking, and indexing functionality; update .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+from scraper import Scraper
 from parser import Parser
+from chunker import Chunker
+from indexer import Indexer
 
-pdf_path = ""
-parser = Parser(pdf_path)
-elements, text = parser.parse()
-print(elements)
-print(text)
+urls = [ 
+    # Current Regulations
+    "https://merc.gov.in/regulation_type/current-regulations-tariff-regulations/",
+    "https://merc.gov.in/regulation_type/current-regulations-renewable-energy/",
+    "https://merc.gov.in/regulation_type/current-regulations-open-access/",
+    "https://merc.gov.in/regulation_type/current-regulations-demand-side-management/",
+    "https://merc.gov.in/regulation_type/current-regulations-grid-operations/",
+
+    # Existing Guidelines
+    "https://merc.gov.in/guideline_type/existing-guidelines/",
+    
+    # Draft Regulations
+    "https://merc.gov.in/regulation_type/draft-regulations/"
+    ]
+
+pdf_paths = set()
+
+for url in urls:
+    scraper = Scraper(base_url=url)
+    save_paths = scraper.scrape()
+    pdf_paths.update(save_paths)
+
+
+for path in pdf_paths:
+    parser = Parser(pdf_path=path)
+    parsed_document = parser.parse()
+    
+    chunker = Chunker(document=parsed_document)
+    chunked_document = chunker.chunk()
+
+    indexer = Indexer(collection="Add Chroma DB Collection Here", chunked_content=chunked_document)
+
