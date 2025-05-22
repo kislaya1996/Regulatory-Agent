21467c746ac4441f2a9eca4e887c83d3ce775bae
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2025-05-22 12:33:22 +0530

    wip

--- tn_orders_main.py
+++ tn_orders_main.py
@@ -4,12 +4,13 @@ from db import DB
 discoms = ["tn_combined"]
 
 questions = [
-    "What are the Fixed Charges for HT and EHV, Industrial and Commercial consumers for 2025-26?",
-    "What are the Energy Charges for HT and EHV, Industrial and Commercial consumers for 2025-26?",
-    "What is the Wheeling Charge for HT and EHV, Industrial and Commercial consumers for 2025-26?",
-    "What is the Wheeling Loss for HT and EHV, Industrial and Commercial consumers for 2025-26?",
-    "What are the CSS for HT and EHV, Industrial and Commercial consumers for 2025-26?",
-    "What are the Green Tariffs for HT and EHV, Industrial and Commercial consumers for 2025-26?"
+    # "What are the Fixed Charges for HT and EHV, Industrial and Commercial consumers for 2025-26?",
+    "What are the Energy Charges and Fixed Charges for all Low Tension (LT) consumers for 2024-25?",
+    "What are the Energy Charges and Demand Charges for all High Tension (HT) consumers for 2024-25?",
+    "What are the Wheeling/Network Charges for HT, LT and Overall for FY 2025-26?",
+    # "What is the Wheeling Loss for HT and EHV, Industrial and Commercial consumers for 2025-26?",
+    "What is the Cross Subsidy Surcharge (CSS) for FY 2024-25?",
+    # "What are the Green Tariffs for HT and EHV, Industrial and Commercial consumers for 2025-26?"
 ]
 
 for discom in discoms:
